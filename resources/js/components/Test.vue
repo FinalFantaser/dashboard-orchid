05acd69c6035d780e979d4e594542a6f55d7eeba
Dmitry G <gorin163@gmail.com>
2022-11-08 18:46:36 +0400

    Дрбавлен VUE 3

---
+++ resources/js/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>TEST FROM VUE 3</h1>
+</template>
+
+<script>
+export default {
+    name: "Test"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/vue.js
@@ -0,0 +1,18 @@
+//import {createApp} from "vue";
+import {createApp} from "vue";
+import Test from "./components/Test.vue";
+
+
+
+
+
+const app = createApp({
+    components:{
+        Test
+    }
+})
+app.mount("#app");
+//app.component('test', Test)
+
+
+
